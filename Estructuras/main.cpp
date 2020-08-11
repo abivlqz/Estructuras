7d190f4f8e258d7f017e48133940f5e211c5eaf9
abivlqz1907 <aby1907@hotmail.com>
2020-08-10 21:18:50 -0600

    Initial Commit

---
+++ Estructuras/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Estructuras
+//
+//  Created by Aby Velazquez on 10/08/20.
+//  Copyright © 2020 Aby Velazquez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
