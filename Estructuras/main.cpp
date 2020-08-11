6ac3c6eaac1775d7d3d2457619611350d033eab9
abivlqz1907 <aby1907@hotmail.com>
2020-08-10 21:26:39 -0600

    Segundo comió de prueba

--- Estructuras/main.cpp
+++ Estructuras/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World Abi!";
+    std::cout << "Hello, World Abi! This is my second commit";
     return 0;
 }
