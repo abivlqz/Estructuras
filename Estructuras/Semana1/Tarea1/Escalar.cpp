63055870905043b0b87ed480e0e6f2e5b6e4093a
abivlqz1907 <aby1907@hotmail.com>
2020-08-12 11:37:25 -0600

    Tareas Terminadas

--- Estructuras/Semana1/Tarea1/Escalar.cpp
+++ Estructuras/Semana1/Tarea1/Escalar.cpp
@@ -1,30 +1,30 @@
-//
 //  Escalar.cpp
 //  Estructuras de Datos
-//  Abigail Guadalupe Velazquez Sanchez
-//  A01566592
-//  Grupo2
-//
+//  Abigail Guadalupe Velazquez Sanchez A01566592 Grupo2
+//  Creado el 11/08/2020
+//  Este programa permite llenar una matriz de orden n solicitada al usuario, y verifica si se trata de una matriz escalar.
 
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-
+//Funcion LLena solicita al usuario llenar una matriz cuadrada de dos dimensiones
+// Sus parametros son una matriz de tipo entero y la dimension de la matriz de tipo entero, no regresa nada.
 void Llena(int matriz[10][10], int dim){
-    
-    
     for (int r = 0; r<dim; r++) {
         for (int c = 0; c<dim; c++) {
             cout<<"Ingresa el elemento de la posicion ["<<r<<" ],["<<c<<"] :"<<endl;
             cin>>matriz[r][c];
         }
     }
-    
 }
 
+// Funcion Calcula compara los elementos diagonales de una matriz cuadrada para
+// identificar si son iguales, sus parametros son una matriz de tipo entero y la dimension
+// de la matriz de tipo entero. Regresa una variable booleana verdadera si sus elementos en la diagonal son iguales.
 bool Calcula(int matriz [10][10], int dim){
+    //Valor inicial con el que se hara la comparacion de elementos en la diagonal
     int valorInicial= matriz[0][0];
     bool escalar = true;
     
@@ -37,13 +37,13 @@ bool Calcula(int matriz [10][10], int dim){
     return escalar;
 }
 
+
 int main(){
     
     int dim;
+    int matriz[10][10];
     cout<<"Ingresa la dimension de tu matriz"<<endl;
     cin>>dim;
-    int matriz[10][10];
-    
     Llena(matriz, dim);
     
     if (Calcula(matriz, dim))
--- Estructuras/Semana1/Tarea1/Pares.cpp
+++ Estructuras/Semana1/Tarea1/Pares.cpp
@@ -1,45 +1,43 @@
-//
 //  Pares.cpp
-//  Abigail Guadalupe Velazquez Sanchez A01566592
-//  Estructuras de datos
-//  Grupo2
-//
-
+//  Estructuras de Datos
+//  Abigail Guadalupe Velazquez Sanchez A01566592 Grupo2
+//  Creado el 11/08/2020
+//  Este programa permite llenar una matriz de orden ncolumnas y nfilas solicitada al usuario, solicita un valor X e imprime en pantalla los pares continuos cuya suma resultante sea X y en que renglon de la matriz se encuentran.
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-//
-void Llena(int matriz[10][10], int dim_r, int dim_c){
-    
-    
-    for (int r = 0; r<dim_r; r++) {
-        for (int c = 0; c<dim_c; c++) {
+// Funcion LLena solicita al usuario llenar una matriz de dos dimensiones
+// Sus parametros son una matriz de tipo entero y  el numero de filas y columnas de la matriz de tipo entero, no regresa nada.
+void Llena(int matriz[10][10], int numRows, int numCol){
+    for (int r = 0; r<numRows; r++) {
+        for (int c = 0; c<numCol; c++) {
             cout<<"Ingresa el elemento de la posicion ["<<r<<" ],["<<c<<"] :"<<endl;
             cin>>matriz[r][c];
         }
     }
-    
 }
 
-void Pares(int matriz[10][10], int dim_r, int dim_c){
-    int X;
+// Funcion Pares compara los pares continuos por renglon de una matriz de n filas y n columnas e imprime en pantalla el par continuo cuya suma sea X, un numero solicitado al usuario, e imprime en que renglon se encontro dicho par.
+// Sus parametros son: una matriz de tipo entero, el numero de filas y el numero de columnas delimitando dicha matriz, y no regresa nada.
+void Pares(int matriz[10][10], int numRows, int numCol){
+    int numX;
     int cont_Pares = 0;
     
-    cout<<"Ingresa el numero X para buscar pares donde su suma resultante sea X "<<endl;
-    cin>>X;
+    cout<<"Ingresa el numero X para buscar pares continuos donde su suma resultante sea X "<<endl;
+    cin>>numX;
     
-    for (int c = 0; c<dim_c-1; c++) {
-        for (int r = 0; r<dim_r; r++) {
-            if ((matriz[r][c]+matriz[r][c+1]) == X) {
+    for (int c = 0; c<numCol-1; c++) {
+        for (int r = 0; r<numRows; r++) {
+            if ((matriz[r][c]+matriz[r][c+1]) == numX) {
                 cout<<"Par encontrado: "<<matriz[r][c]<<","<< matriz[r][c+1]<<" en el renglon "<<r<<endl;
                 cont_Pares++;
             }
         }
     }
     if(cont_Pares == 0){
-        cout<<"No se encontro ningun par continuo en la matriz cuya suma resultante fuera "<<X<<endl;
+        cout<<"No se encontro ningun par continuo en la matriz cuya suma resultante fuera "<<numX<<endl;
     }
 }
 
@@ -56,5 +54,5 @@ int main(){
     Llena(matriz, filas, columnas);
     Pares(matriz, filas, columnas);
     
-    
+    return 0;
 }
