45f68d01bf3bc010c74e68a454e48afb17adb120
Abigail Velazquez <aby1907@hotmail.com>
2020-11-09 08:18:55 -0700

    act grafos dependencias

---
+++ Estructuras/Grafos/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  Estructuras
+//
+//  Created by Aby Velazquez on 09/11/20.
+//  Copyright © 2020 Aby Velazquez. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+
+int sumaDependencias(char mat[MAX][MAX], int numNodos){
+    int sumDep=0;
+    for(int i=0;i<numNodos;i++){
+        for(int j=0;j<numNodos;j++){
+            if(mat[i][j]== 'T')
+                sumDep+=1;
+        }
+    }
+    return sumDep;
+}
+
+int main()
+{
+    char matriz[MAX][MAX];
+    int numNodos;
+    cout<<"Cuantos nodos hay en el grafo"<<endl;
+    cin>>numNodos;
+    for(int i=0;i<numNodos;i++){
+        for(int j=0;j<numNodos;j++){
+        cout<<"Escribe la dependencia ("<<i<<","<<j<<")"<<endl;
+        cin>>matriz[i][j];
+        }
+    }
+
+    cout<<"El numero de dependencias es: "<<sumaDependencias(matriz,numNodos)<<endl;
+
+    return 0;
+}
+
